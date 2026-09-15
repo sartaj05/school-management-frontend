777eba5fa0f64493dbcdb2a51ac744ba13761761
sartaj <sartajahamad42889@gmail.com>
2026-09-15 15:45:41 +0530

    build: update Vite chunk size warning limit to 700 kB

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,18 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 5173,
-    proxy: { '/api': { target: 'http://127.0.0.1:5000', changeOrigin: true } },
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+      },
+    },
+  },
+
+  build: {
+    chunkSizeWarningLimit: 700,
   },
 })
